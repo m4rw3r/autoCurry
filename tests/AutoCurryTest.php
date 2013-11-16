719b64394b124d3c25ff85f4a64fda88dcbfcf9f
Martin Wernstål <m4rw3r@gmail.com>
2013-11-16 01:05:53 +0100

    Tests: Added tests for curryed function being invoked with multiple parameters simultaneously

--- tests/AutoCurryTest.php
+++ tests/AutoCurryTest.php
@@ -74,6 +74,55 @@ class AutoCurryTest extends \PHPUnit_Framework_TestCase
 		$this->assertSame($f3($dummy3), [$dummy1, $dummy2, $dummy3]);
 	}
 
+	public function testMultipleParamsAtOneTime1()
+	{
+		$dummy1 = new stdClass();
+		$dummy2 = new stdClass();
+
+		$f = autoCurry(function($a, $b) { return [$a, $b]; });
+
+		$this->assertTrue(is_callable($f));
+		$this->assertSame($f($dummy1, $dummy2), [$dummy1, $dummy2]);
+	}
+
+	public function testMultipleParamsAtOneTime2()
+	{
+		$dummy1 = new stdClass();
+		$dummy2 = new stdClass();
+		$dummy3 = new stdClass();
+
+		$f = autoCurry(function($a, $b, $c) { return [$a, $b, $c]; });
+
+		$this->assertTrue(is_callable($f));
+
+		$f2 = $f($dummy1, $dummy2);
+
+		$this->assertTrue(is_callable($f2));
+		$this->assertSame($f2($dummy3), [$dummy1, $dummy2, $dummy3]);
+	}
+
+	public function testMultipleParamsAtOneTime3()
+	{
+		$dummy1 = new stdClass();
+		$dummy2 = new stdClass();
+		$dummy3 = new stdClass();
+		$dummy4 = new stdClass();
+		$dummy5 = new stdClass();
+
+		$f = autoCurry(function($a, $b, $c, $d, $e) { return [$a, $b, $c, $d, $e]; });
+
+		$this->assertTrue(is_callable($f));
+
+		$f2 = $f($dummy1, $dummy2);
+
+		$this->assertTrue(is_callable($f2));
+
+		$f3 = $f($dummy3, $dummy4);
+
+		$this->assertTrue(is_callable($f3));
+		$this->assertSame($f2($dummy5), [$dummy1, $dummy2, $dummy3, $dummy4, $dummy5]);
+	}
+
 	public function testStaticFunction1()
 	{
 		$dummy1 = new stdClass();
